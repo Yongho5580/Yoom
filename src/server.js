ad2acaceeb7ff4b8d28cb295f48838d7347f902b
Yongho5580 <Yongho5580@users.noreply.github.com>
2021-09-09 19:23:37 +0900

    create server.js

--- src/server.js
+++ src/server.js
@@ -2,6 +2,11 @@ import express from "express";
 
 const app = express();
 
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+
 console.log("hello world");
 
-app.listen(3000);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+
+app.listen(3000, handleListen);
